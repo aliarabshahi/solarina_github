6f264a3ecff2a72269d1f427b73b186da54c9b49
user <user@users-MacBook-Pro.local>
2026-04-30 07:24:22 +0330

    adding enamd

--- frontend/src/app/components/footer/FooterSocial.tsx
+++ frontend/src/app/components/footer/FooterSocial.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import { FaInstagram, FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
-import { FaSun } from "react-icons/fa";
+import { FaInstagram, FaTelegramPlane, FaWhatsapp, FaSun } from "react-icons/fa";
 
 const socialLinks = [
   {
     name: "اینستاگرام",
-    href: "https://instagram.com/yourpage",
+    href: "https://instagram.com/yourpagee",
     icon: <FaInstagram className="text-[#E4405F] text-2xl sm:text-[26px]" />,
   },
   {
@@ -23,10 +22,26 @@ const socialLinks = [
 export default function FooterSocial() {
   return (
     <nav className="flex flex-col items-center space-y-3">
-      <div className="relative pb-2 pt-2 z-10 flex justify-center items-center">
+
+      {/* Logo + Enamad */}
+      <div className="relative pb-2 pt-2 z-10 flex justify-center items-center gap-4">
         <Link href="/" aria-label="صفحه اصلی">
           <FaSun className="text-yellow-400 w-14 h-14 sm:w-20 sm:h-20 md:w-24 md:h-24 transition-all duration-200" />
         </Link>
+
+        <a
+          referrerPolicy="origin"
+          target="_blank"
+          href="https://trustseal.enamad.ir/?id=722392&Code=k5XI9hwNFKb97I6nCJckpbG4w29I6H3I"
+        >
+          <img
+            referrerPolicy="origin"
+            src="https://trustseal.enamad.ir/logo.aspx?id=722392&Code=k5XI9hwNFKb97I6nCJckpbG4w29I6H3I"
+            alt="نماد اعتماد الکترونیکی"
+            className="w-14 h-14 sm:w-20 sm:h-20 md:w-24 md:h-24 object-contain"
+            style={{ cursor: "pointer" }}
+          />
+        </a>
       </div>
 
       <h6 className="footer-title text-base sm:text-lg font-bold text-center">
@@ -47,6 +62,7 @@ export default function FooterSocial() {
           </Link>
         ))}
       </div>
+
     </nav>
   );
 }
