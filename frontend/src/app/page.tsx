7dc43e0a2a2734493eb8b5c832b28097fecf05d1
user <user@users-MacBook-Pro.local>
2026-06-26 08:59:15 +0330

    fix the bug of page not fit in mobile size

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative overflow-hidden">
       <Hero />
     </main>
   );
